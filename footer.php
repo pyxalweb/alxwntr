917930ac25538b8abcd88189f6e3a42db6583919
Alex Winter <alex@pyxalweb.com>
2021-07-10 00:13:38 -0700

    Minor CSS update and edit to social media stuff
    Set height: 100%; for image elements within blog posts so that they display correctly on smaller viewports.
    
    Also rearranged and added some social media crap.

--- footer.php
+++ footer.php
@@ -3,11 +3,6 @@
 
 		<ul class="social">
 			<li>
-				<a class="reddit" aria-label="Reddit" href="https://www.reddit.com/user/aydoubleyou" target="_blank" rel="noopener">
-					<span class="fab fa-reddit-square"></span>
-				</a>
-			</li>
-			<li>
 				<a class="instagram" aria-label="Instagram" href="https://www.instagram.com/alx.wntr/" target="_blank" rel="noopener">
 					<span class="fab fa-instagram-square"></span>
 				</a>
@@ -18,6 +13,11 @@
 				</a>
 			</li>
 			<li>
+				<a class="reddit" aria-label="Reddit" href="https://www.reddit.com/user/aydoubleyou" target="_blank" rel="noopener">
+					<span class="fab fa-reddit-square"></span>
+				</a>
+			</li>
+			<li>
 				<a class="twitter" aria-label="Twitter" href="https://twitter.com/aydoubleyou" target="_blank" rel="noopener">
 					<span class="fab fa-twitter-square"></span>
 				</a>
@@ -25,6 +25,21 @@
 			<li>
 				<a class="linkedin" aria-label="LinkedIn" href="https://www.linkedin.com/in/alex-winter-38696626/" target="_blank" rel="noopener">
 					<span class="fab fa-linkedin"></span>
+				</a>
+			</li>
+			<li>
+				<a class="github" aria-label="GitHub" href="https://github.com/pyxalweb" target="_blank" rel="noopener">
+					<span class="fab fa-github-square"></span>
+				</a>
+			</li>
+			<li>
+				<a class="lastfm" aria-label="Last.fm" href="https://www.last.fm/user/aydoubleyou" target="_blank" rel="noopener">
+					<span class="fab fa-lastfm-square"></span>
+				</a>
+			</li>
+			<li>
+				<a class="spotify" aria-label="Spotify" href="https://open.spotify.com/user/aydoubleyou" target="_blank" rel="noopener">
+					<span class="fab fa-spotify"></span>
 				</a>
 			</li>
 		</ul>

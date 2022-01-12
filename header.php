e1e5a793d44acac9082408c8ce75c29617749e06
Alex Winter <alex@pyxalweb.com>
2022-01-11 22:40:16 -0800

    Analytics and CSS
    * Added Google Analytics :/
    * Minor CSS adjustments.

--- header.php
+++ header.php
@@ -4,6 +4,16 @@
 	<meta charset="<?php bloginfo('charset'); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<?php wp_head(); ?>
+
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=G-GMLJF7GTXY"></script>
+	<script>
+		window.dataLayer = window.dataLayer || [];
+		function gtag(){dataLayer.push(arguments);}
+		gtag('js', new Date());
+
+		gtag('config', 'G-GMLJF7GTXY');
+	</script>
 </head>
 <body <?php body_class(); ?>>
 	<header class="site-header" id="header">
